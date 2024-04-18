2166ad42a94fd51af6dec94d0b45db67f7747de7
Mavrik <mrricki.m.usmc@gmail.com>
2024-04-18 11:01:24 +0200

    fix: remove unused api functions

--- src/api/lighthouse.ts
+++ src/api/lighthouse.ts
@@ -1,20 +1,5 @@
 import axios from 'axios'
 
-export const fetchVersion = async (baseUrl: string, apiToken: string) =>
-  await axios.get(`${baseUrl}/version`, {
-    headers: {
-      authorization: `Bearer ${apiToken}`,
-    },
-  })
-
-export const fetchValidators = async (baseUrl: string, token: string) =>
-  await axios.get(`${baseUrl}/validators`, {
-    headers: { Authorization: `Bearer ${token}` },
-  })
-
-export const fetchValidatorCount = async (baseUrl: string) =>
-  await axios.get(`${baseUrl}/lighthouse/ui/validator_count`)
-
 export const fetchValidatorGraffiti = async (baseValidatorUrl: string, token: string) =>
   await axios.get(`${baseValidatorUrl}/ui/graffiti`, {
     headers: { Authorization: `Bearer ${token}` },
