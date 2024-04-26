575540d36f8f8a7831b2fabee2f3561a610f4f93
Mavrik <mrricki.m.usmc@gmail.com>
2024-04-26 18:04:59 +0200

    feat: added new func to util service

--- backend/src/utils/utils.service.ts
+++ backend/src/utils/utils.service.ts
@@ -1,5 +1,5 @@
 // utils.service.ts
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { firstValueFrom, Observable } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
 import { AxiosRequestConfig, AxiosResponse, Method } from 'axios';
@@ -7,6 +7,8 @@ import { StatusColor } from '../../../src/types';
 import { DiagnosticRate } from '../../../src/constants/enums';
 import { Request, Response } from 'express';
 import * as EventSource from 'eventsource';
+import { ModelCtor, Model } from 'sequelize-typescript';
+import { FindOptions } from 'sequelize';
 
 @Injectable()
 export class UtilsService {
@@ -73,5 +75,43 @@ export class UtilsService {
       eventSource.close();
       res.end();
     });
+  }
+
+  fetchOne<T extends Model>(model: ModelCtor<T>): Promise<T> {
+    return this.fetchDataWithRetry(() => model.findOne()) as Promise<T>
+  }
+
+  async fetchAll<T extends Model>(model: ModelCtor<T>, options?: FindOptions): Promise<T[]> {
+    const data = await this.fetchDataWithRetry(() => model.findAll(options)) as T[]
+    return data.map(item => item.dataValues)
+  }
+
+  async fetchDataWithRetry<T extends Model>(fetchFunction: () => Promise<T | T[] | null>, maxRetries: number = 3): Promise<T | T[] | null> {
+    let attempts = 0;
+    while (attempts < maxRetries) {
+      try {
+        return await fetchFunction();
+      } catch (error) {
+        if (this.isLockError(error)) {
+          attempts++;
+          await this.wait(this.getExponentialBackoffTime(attempts));
+        } else {
+          throw new HttpException('Database Error', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+      }
+    }
+    throw new HttpException('Request timed out due to database lock, please retry later.', HttpStatus.REQUEST_TIMEOUT);
+  }
+
+  private isLockError(error: any): boolean {
+    return error.message.includes('lock') || error.name === 'SequelizeDatabaseError';
+  }
+
+  private wait(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  private getExponentialBackoffTime(attempt: number): number {
+    return Math.pow(2, attempt) * 100;  // Base time is 100 ms
   }
 }
