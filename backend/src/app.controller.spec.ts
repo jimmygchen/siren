5cc75c9da7f02343b8019d5b08a512061a1bcd67
Mavrik <mrricki.m.usmc@gmail.com>
2024-05-22 10:56:46 +0200

    feat: added unit test for app controller auth

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        JwtModule.register({
+          global: true,
+          secret: 'fake-value',
+          signOptions: { expiresIn: '7200s' }, //set to  2 hours
+        }),
+      ],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -14,9 +23,12 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('root auth', () => {
+    it('should reject invalid passwords', async () => {
+      await expect(appController.authenticate({password: ''})).rejects.toThrow(new UnauthorizedException('Invalid session password...'));
+    });
+    it('should return valid session tokens', async () => {
+      expect(await appController.authenticate({password: process.env.SESSION_PASSWORD})).toMatchObject({ access_token: expect.any(String) });
     });
   });
 });
