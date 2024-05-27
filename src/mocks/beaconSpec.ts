db7f411dbc6a10f4bbba8cc0c65094adeed7e2af
Mavrik <mrricki.m.usmc@gmail.com>
2024-05-27 13:35:43 +0200

    feat: added to node controller tests

--- src/mocks/beaconSpec.ts
+++ src/mocks/beaconSpec.ts
@@ -11,30 +11,30 @@ export const mockBeaconSpec = {
 
 export const mockDiagnostics = {
   totalDiskSpace: 123,
-  diskUtilization: 123,
+  diskUtilization: 0,
   totalDiskFree: 123,
   diskStatus: {
     synced: StatusColor.ERROR,
-    syncing: StatusColor.ERROR
+    syncing: StatusColor.SUCCESS
   },
   totalMemory: 123,
-  memoryUtilization: 123,
-  frequency: '10hz',
-  ramStatus: StatusColor.ERROR,
-  cpuStatus: StatusColor.ERROR,
-  cpuUtilization: '19gb',
-  networkName: 'exmple',
+  memoryUtilization: 0,
+  frequency: "100",
+  ramStatus: StatusColor.SUCCESS,
+  cpuStatus: StatusColor.SUCCESS,
+  cpuUtilization: '6.0',
+  networkName: 'example',
   natOpen: false,
   uptime: {
-    beacon: '10m',
-    validator: '19m'
+    beacon: "10M 0S",
+    validator: "10M 0S"
   },
   healthCondition: {
-    synced: DiagnosticRate.FAIR,
-    syncing: DiagnosticRate.FAIR
+    synced: DiagnosticRate.POOR,
+    syncing: DiagnosticRate.GOOD
   },
   overallHealthStatus: {
     synced: StatusColor.ERROR,
-    syncing: StatusColor.ERROR
+    syncing: StatusColor.SUCCESS
   }
 }
